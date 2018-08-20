e9b681c3d8b1de0cbfcb5d1cc26ec5cb1ba53348
Maciej Duda <maciej.duda1@gazeta.pl>
2018-08-20 22:16:12 +0200

    work work

--- src/app/header/services/get-weather.service.ts
+++ src/app/header/services/get-weather.service.ts
@@ -1,3 +1,4 @@
+import { Location } from './get-weather.service';
 import { Http, Response } from '@angular/http';
 import 'rxjs/Rx';
 import { Injectable } from '@angular/core';
@@ -9,44 +10,18 @@ export interface Location {
 
 @Injectable()
 export class GetWeatherService {
-  location: Location;
+
   constructor (private http: Http) {}
 
-  getLocation() {
-    navigator.geolocation.getCurrentPosition( position => {
-      console.log(position);
-      this.location = {
-        latitude: position.coords.latitude,
-        longtitude: position.coords.longitude };
-      });
-  }
-
-  getWeather() {
-    const Url = 'https://www.metaweather.com//api/location/search/?lattlong=' + this.location.latitude + '' + this.location.longtitude;
-
+  getWeather(location: Location) {
+    const Url = 'https://www.metaweather.com//api/location/search/?lattlong=' + location.latitude + ',' + location.longtitude;
+    console.log('jestem: ',  Url);
     this.http.get(Url)
-      .map(
+      .subscribe(
         (response: Response) => {
-          console.log(response);
+          const data = response.json();
+          console.log(data);
         }
       );
   }
-
-//   showError(error) {
-//     switch(error.code) {
-//         case error.PERMISSION_DENIED:
-//             console.log('User denied the request for Geolocation.');
-//             break;
-//         case error.POSITION_UNAVAILABLE:
-//             console.log('Location information is unavailable.');
-//             break;
-//         case error.TIMEOUT:
-//             console.log('The request to get user location timed out.');
-//             break;
-//         case error.UNKNOWN_ERROR:
-//             console.log('An unknown error occurred.');
-//             break;
-//     }
-// }
-
 }
--- src/app/header/weather/weather.component.ts
+++ src/app/header/weather/weather.component.ts
@@ -8,15 +8,42 @@ import { GetWeatherService, Location } from '../services/get-weather.service';
 })
 export class WeatherComponent implements OnInit {
 
-  position: Location;
+  position: Location = {latitude: 0, longtitude: 0};
 
   constructor(private getWeatherService: GetWeatherService) { }
 
   ngOnInit() {
-   this.getWeatherService.getWeather();
+    this.getLocation();
   }
 
+  getLocation() {
+    navigator.geolocation.getCurrentPosition (
+      location => {
+        console.log(location.coords.latitude, location.coords.longitude);
+        this.position.latitude = location.coords.latitude;
+        this.position.longtitude = location.coords.longitude;
+        this.getWeatherService.getWeather(this.position);
+      },
+      error => this.showError(error)
+    );
+  }
 
+  showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            console.log('User denied the request for Geolocation.');
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log('Location information is unavailable.');
+            break;
+        case error.TIMEOUT:
+            console.log('The request to get user location timed out.');
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log('An unknown error occurred.');
+            break;
+    }
+}
 
 
 }
